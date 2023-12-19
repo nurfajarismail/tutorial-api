274730d1b0b48eaa6869a201e25b61c3fde19cda
Nur Fajar Ismail <fadjarismail0@gmail.com>
2023-12-19 16:13:57 +0700

    create table comments, users, and posts

--- database/migrations/2023_12_19_090455_create_posts_table.php
+++ database/migrations/2023_12_19_090455_create_posts_table.php
@@ -10,15 +10,11 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('username')->unique();
-            $table->string('firstname', 100);
-            $table->string('lastname', 100)->nullable();
-            $table->string('password');
-
+            $table->string('title', 255);
+            $table->string('news_content');
+            $table->foreignId('author')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
             $table->softDeletes();
         });
@@ -29,6 +25,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
